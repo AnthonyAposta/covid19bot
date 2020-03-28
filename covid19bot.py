ba7d0ded71d2f8268a6ba2d71307f57313fae38d
romcenci <romcenci@gmail.com>
2020-03-27 23:26:01 -0300

    /info more fast, retrieving of data just in the bot start

--- covid19bot.py
+++ covid19bot.py
@@ -1,9 +1,13 @@
 # TODO or ideas
-# - [ ] At principle options are: /help, /graph, /info
-# - [ ] /map option
-# - [ ] /news
+# - [x] /info
+#   - [ ] top countries infected and deltas in /info
+# - [ ] /graph
+# - [ ] /help
+# - [ ] /map
 # - [ ] /daily_subscribe
+# - [ ] /news
 
+import COVID19Py
 import telepot
 import os
 import subprocess
@@ -12,7 +16,14 @@ import time
 token = os.getenv("COV19BOT_TOKEN")
 bot = telepot.Bot(token)
 
-add = ""
+covid19 = COVID19Py.COVID19(data_source="jhu")
+
+allData = covid19.getAll()
+total = allData['latest']
+locations = allData['locations']
+ids = [country['country_code'] for country in locations]
+
+print("Ready: Database updated")
 
 def recebendoMsg(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
@@ -22,28 +33,26 @@ def recebendoMsg(msg):
         bot.sendMessage(chat_id, 'Hello '+msg['from']['first_name']+', I can show you the stats of the covid19 from all around the world.')
 ##########################################
     if msg['text'].split(' ',1)[0] == '/graph':
-        bot.sendMessage(chat_id, 'Ok, I\'m doing it...')
+        bot.sendChatAction(chat_id, 'upload_photo')
         if len(msg['text'].split(' ',1)) == 2:
             bot.sendPhoto(chat_id, open(msg['text'].split(' ',1)[1]+".jpg",'rb'))
         else:
             bot.sendPhoto(chat_id, open("world.png",'rb'))
 ##########################################
     if msg['text'].split(' ',1)[0] == '/info':
-        bot.sendMessage(chat_id, 'Ok, I\'m doing it...')
-        if len(msg['text'].split(' ',1)) == 2 and msg['text'].split(' ',1)[1] == 'BR':
+        bot.sendChatAction(chat_id, 'typing')
+        if len(msg['text'].split(' ',1)) == 2: # Filtered by country code
+            countryID=ids.index(msg['text'].split(' ',1)[1])
+            
             bot.sendMessage(chat_id, parse_mode='Markdown', text='\
-            *Brazil*\
-            \n- Confirmed cases today:\
-            \n- Death cases today:\
-            \n- Confirmed cases cumulated:\
-            \n- Death cases cumulated:')
-        else:
+            *'+str(locations[countryID]['country'])+'*\
+            \n- Total confirmed cases until today: '+str(locations[countryID]['latest']['confirmed'])+'\
+            \n- Total deaths until today:  '+str(locations[countryID]['latest']['deaths']))
+        else: # World total cases
             bot.sendMessage(chat_id, parse_mode='Markdown', text='\
             *World*\
-            \n- Confirmed cases today:\
-            \n- Death cases today:\
-            \n- Confirmed cases cumulated:\
-            \n- Death cases cumulated:')
+            \n- Total confirmed cases until today: '+str(total['confirmed'])+'\
+            \n- Total deaths until today: '+str(total['deaths']))
 ##########################################
 
 bot.message_loop(recebendoMsg)
