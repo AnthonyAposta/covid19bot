8ff12e9b2444ce7312cfbed684eca0b7200b3794
anthony <anthonyjbet@gmail.com>
2020-03-28 01:04:00 -0300

    add oop

--- covid19bot.py
+++ covid19bot.py
@@ -12,16 +12,14 @@ import telepot
 import os
 import subprocess
 import time
+from database import Database
+
+
 
 token = os.getenv("COV19BOT_TOKEN")
 bot = telepot.Bot(token)
 
-covid19 = COVID19Py.COVID19(data_source="jhu")
-
-allData = covid19.getAll()
-total = allData['latest']
-locations = allData['locations']
-ids = [country['country_code'] for country in locations]
+DADOS = Database()
 
 print("Ready: Database updated")
 
@@ -42,17 +40,17 @@ def recebendoMsg(msg):
     if msg['text'].split(' ',1)[0] == '/info':
         bot.sendChatAction(chat_id, 'typing')
         if len(msg['text'].split(' ',1)) == 2: # Filtered by country code
-            countryID=ids.index(msg['text'].split(' ',1)[1])
+            countryID=DADOS.ids.index(msg['text'].split(' ',1)[1])
             
             bot.sendMessage(chat_id, parse_mode='Markdown', text='\
-            *'+str(locations[countryID]['country'])+'*\
-            \n- Total confirmed cases until today: '+str(locations[countryID]['latest']['confirmed'])+'\
-            \n- Total deaths until today:  '+str(locations[countryID]['latest']['deaths']))
+            *'+str(DADOS.locations[countryID]['country'])+'*\
+            \n- Total confirmed cases until today: '+str(DADOS.locations[countryID]['latest']['confirmed'])+'\
+            \n- Total deaths until today:  '+str(DADOS.locations[countryID]['latest']['deaths']))
         else: # World total cases
             bot.sendMessage(chat_id, parse_mode='Markdown', text='\
             *World*\
-            \n- Total confirmed cases until today: '+str(total['confirmed'])+'\
-            \n- Total deaths until today: '+str(total['deaths']))
+            \n- Total confirmed cases until today: '+str(DADOS.total['confirmed'])+'\
+            \n- Total deaths until today: '+str(DADOS.total['deaths']))
 ##########################################
 
 bot.message_loop(recebendoMsg)
--- database.py
+++ database.py
@@ -8,24 +8,32 @@ import COVID19Py
 class Database:
 
 	def __init__(self):
-	
-		self.data = COVID19Py.COVID19(data_source="jhu").getLocations(timelines=True)
-	
-		self.IDs = [country['country_code'] for country in self.data]
+
+		covid19 = COVID19Py.COVID19(data_source="jhu")
+
+		self.allData = covid19.getAll()
+		self.total = self.allData['latest']
+		self.locations = self.allData['locations']
+		self.ids = [country['country_code'] for country in self.locations]
 
 
 	def update_database(self):
-		self.data = COVID19Py.COVID19().getLocations(timelines=True)
+
+		covid19 = COVID19Py.COVID19(data_source="jhu")
+
+		self.allData = covid19.getAll()
+		self.total = self.allData['latest']
+		self.locations = self.allData['locations']
+		self.ids = [country['country_code'] for country in self.locations]
 
 
 
 class Chart:
 	
 	""" usa os argumentos para gerar um grafico """
-	def __init__(self, data, index, escala = 'linear', tipo = 'acumulativo' ):
+	def __init__(self, Locations, ids, index):
 		
-
-		self.Locations = data
+		self.Locations = Locations
 
 		index = int(index)
 		escala = str(escala)
@@ -35,22 +43,17 @@ class Chart:
 		self.fig = plt.figure(num=1, figsize = (10., 8.))
 		self.ax  = self.fig.add_subplot(1,1,1)
 
-
-		if escala == 'linear' and tipo == 'acumulativo':
-			self.chart = self.linear_acumulativo(index)
-			plt.savefig(f'test_{index}',bbox_inches='tight')
-		else:
-			pass
-
-		#plt.show()
+		self.chart = self.linear_acumulativo(index)
+		plt.savefig(f'test_{ids[index]}',bbox_inches='tight')
 		
+
 
 	def get_data(self, index):
 		""" pega os dados de infectados vs dias para o pais 'lugar' """
 
 		data = self.Locations[index]['timelines']['confirmed']['timeline']
 
-		return np.array([dia for dia in data]), np.array([data[dia] for dia in data])
+		return np.array([dia[2:10] for dia in data]), np.array([data[dia] for dia in data])
 
 
 	def linear_acumulativo(self, index):
@@ -64,6 +67,3 @@ class Chart:
 		plt.xticks(rotation=90, size='medium')
 
 
-Data = Database()
-
-chart = Chart(Data.data,0)
