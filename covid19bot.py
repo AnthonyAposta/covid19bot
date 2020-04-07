a581c1c270218b3995591f69b00816861c8c8de6
romcenci <romcenci@gmail.com>
2020-04-07 20:03:09 -0300

    Update covid19bot.py

--- covid19bot.py
+++ covid19bot.py
@@ -344,7 +344,7 @@ def on_chat_message(msg):
 
 
 # schedule to send message every day at 11:00 UTC 
-schedule.every().day.at("22:40").do(send_subscribe_msg)
+schedule.every().day.at("23:10").do(send_subscribe_msg)
 
 MessageLoop(bot, {'chat': on_chat_message,
                   'callback_query': on_callback_query}).run_as_thread()
