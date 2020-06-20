ef0d109683d9be17297c4c751c869800610bd3ce
anthony <anthonyjbet@gmail.com>
2020-06-20 13:24:30 -0300

    change chart2 to 7 days

--- covid19bot.py
+++ covid19bot.py
@@ -16,10 +16,9 @@ import schedule
 
 token = os.getenv("COV19BOT_TOKEN")
 
-
 bot = telepot.Bot(token)
 
-#SUBS = subs_db()
+SUBS = subs_db()
 print("* Subscribers database updated")
 
 DADOS = Database()
