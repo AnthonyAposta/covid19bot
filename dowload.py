e2276d0215fec205a5839a4ff7cbc1edecdfb1ca
anthony <anthonyjbet@gmail.com>
2020-06-22 16:54:59 -0300

    sched

--- dowload.py
+++
@@ -1,7 +0,0 @@
-import COVID19Py
-import numpy as np
-
-def dowload_data():
-    covid19 = COVID19Py.COVID19(data_source="jhu")
-    data = np.array([covid19.getAll(timelines=True)])
-    np.save('new_data.npy', data)
